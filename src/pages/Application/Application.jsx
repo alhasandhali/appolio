699a04d8abba919ed86da2731c31da13d43445c3
Al Hasan Dhali <alhasandhali@gmail.com>
2025-10-17 09:21:03 +0600

    final touch

--- src/pages/Application/Application.jsx
+++ src/pages/Application/Application.jsx
@@ -19,20 +19,27 @@ const Application = ({ app }) => {
 
   return (
     <Link to={`/app-details/${app.id}`}>
-      <div className="card bg-base-100 w-[300px] shadow-sm p-3">
-        <figure>
-          <img className="h-64" src={app.image} alt="app_logo" />
+      <div className="card bg-base-100 w-full h-full shadow-sm p-3">
+        <figure className="flex justify-center">
+          <img
+            className="h-full sm:h-56 md:h-64 object-contain"
+            src={app.image}
+            alt="app_logo"
+          />
         </figure>
-        <div className="">
-          <h2 className="my-4 inter font-medium text-xl text-[#001931]">
+
+        <div>
+          <h2 className="my-3 sm:my-4 inter font-medium text-lg sm:text-xl text-[#001931] text-center sm:text-left">
             {app.title}
           </h2>
-          <div className="card-actions justify-between">
-            <div className="badge bg-[#F1F5E8] inter font-medium text-[16px] text-[#00D390]">
+
+          <div className="card-actions flex flex-wrap justify-center sm:justify-between gap-2">
+            <div className="badge flex items-center gap-1 bg-[#F1F5E8] inter font-medium text-sm sm:text-[16px] text-[#00D390]">
               <img className="h-4" src={downloadIcon} alt="download" />
               {formatNumber(app.downloads)}
             </div>
-            <div className="badge bg-[#FFF0E1] inter font-medium text-[16px] text-[#FF8811]">
+
+            <div className="badge flex items-center gap-1 bg-[#FFF0E1] inter font-medium text-sm sm:text-[16px] text-[#FF8811]">
               <img className="h-4" src={ratingIcon} alt="rating" />
               {app.ratingAvg}
             </div>
